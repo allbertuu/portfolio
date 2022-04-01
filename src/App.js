33e88ff0bc1deb2352754aefb9faffd18868300d
Alberto <albertovinicius3@gmail.com>
2022-04-01 11:11:24 -0300

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import Button from './components/Button';
 import NavItem from './components/NavItem';
-import sr from './assets/scripts/ScrollReveal'
 
+import Home from './pages/Home';
+
+import sr from './assets/scripts/ScrollReveal';
 import './App.scss';
 
 function App() {
@@ -18,35 +24,8 @@ function App() {
       scale: 1,
       easing: 'ease',
     }
-    const item_1 = {
-      origin: 'top',
-      duration: 1500,
-      delay: 150,
-      distance: '50px',
-      scale: 1,
-      easing: 'ease',
-    }
-    const item_2 = {
-      origin: 'top',
-      duration: 1500,
-      delay: 2000,
-      distance: '20px',
-      scale: 1,
-      easing: 'ease',
-    }
-    const item_3 = {
-      origin: 'left',
-      duration: 2000,
-      delay: 4000,
-      distance: '50px',
-      scale: 1,
-      easing: 'ease',
-    }
 
     sr.reveal('.box', config);
-    sr.reveal('.item_1', item_1);
-    sr.reveal('.item_2', item_2);
-    sr.reveal('.item_3', item_3);
   }, []);
 
   return (
@@ -64,29 +43,13 @@ function App() {
         </nav>
       </header>
 
-      {/* aqui vai começar as rotas - pages */}
-      <main>
-        <section className='l_welcome'>
-          <div className="welcome">
-            <span className="item_1">Olá!</span>
-            <span className="item_2">Me chamo Alberto.</span>
-            <span className="item_3">Muito prazer o/</span>
-          </div>
-          <div>
-            <Button text="Quero conhecê-lo!" />
-          </div>
-        </section>
-
-        <section className='l_aboutMe'>
-          <div className="box">
-            Box
-          </div>
-          <div className="aboutMe">
-            Sobre mim
-          </div>
-        </section>
-      </main>
-      {/* aqui vai terminar as rotas - pages */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/empresa" element={<Empresa />} />
+          <Route path="/contato" element={<Contato />} /> */}
+        </Routes>
+      </Router>
 
       <footer></footer>
     </>
---
+++ src/pages/Home.js
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+
+import Button from '../components/Button';
+
+import sr from '../assets/scripts/ScrollReveal';
+import '../assets/sass/Home.scss';
+
+function Home() {
+
+  useEffect(() => {
+
+    const item_1 = {
+      origin: 'top',
+      duration: 1500,
+      delay: 150,
+      distance: '50px',
+      scale: 1,
+      easing: 'ease',
+    }
+    const item_2 = {
+      origin: 'top',
+      duration: 1500,
+      delay: 2000,
+      distance: '20px',
+      scale: 1,
+      easing: 'ease',
+    }
+    const item_3 = {
+      origin: 'left',
+      duration: 2000,
+      delay: 4000,
+      distance: '50px',
+      scale: 1,
+      easing: 'ease',
+    }
+
+    sr.reveal('.item_1', item_1);
+    sr.reveal('.item_2', item_2);
+    sr.reveal('.item_3', item_3);
+  }, []);
+
+  return (
+    <main>
+      <section className='l_welcome'>
+        <div className="welcome">
+          <span className="item_1">Olá!</span>
+          <span className="item_2">Me chamo Alberto.</span>
+          <span className="item_3">Muito prazer o/</span>
+        </div>
+        <div>
+          <Button text="Quero conhecê-lo!" />
+        </div>
+      </section>
+
+      <section className='l_aboutMe'>
+        <div className="box">
+          Box
+        </div>
+        <div className="aboutMe">
+          Sobre mim
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Home
