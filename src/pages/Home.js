f760909ebd7b78a6c8ec261886be502668925fa5
Alberto <albertovinicius3@gmail.com>
2022-04-06 11:55:07 -0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,30 +14,21 @@ function Home() {
 
     const item_1 = {
       origin: 'top',
-      duration: 1500,
-      delay: 150,
-      distance: '50px',
-      easing: 'ease',
+      delay: 200,
     }
     const item_2 = {
       origin: 'top',
-      duration: 1500,
-      delay: 2000,
-      distance: '20px',
-      easing: 'ease',
+      delay: 2200,
     }
     const item_3 = {
-      origin: 'left',
-      duration: 2000,
+      origin: 'top',
       delay: 4000,
-      distance: '50px',
-      easing: 'ease',
     }
     const item_4 = {
-      duration: 1000,
+      origin: 'bottom',
+      duration: 850,
       delay: 6000,
-      distance: '100px',
-      scale: 0.6,
+      scale: 0.5,
       easing: 'ease',
     }
 
@@ -45,35 +36,38 @@ function Home() {
     sr.reveal('.item_1', item_1);
     sr.reveal('.item_2', item_2);
     sr.reveal('.item_3', item_3);
-    sr.reveal('.item_4', item_4);
+    sr.reveal('.btn_wantToMeetYou', item_4);
 
   }, []);
 
   return (
     <main>
-      <section className='l_welcome'>
-        <div className="welcome">
+      <section className='s_welcome'>
+        <div className="welcome_text">
           <span className="item_1">Olá!</span>
-          <span className="item_2">Me chamo Alberto.</span>
+          <span className="item_2">Me chamo Alberto</span>
           <span className="item_3">Muito prazer o/</span>
         </div>
-        <div className='item_4'>
+        <div className='btn_wantToMeetYou'>
           <Button text="Quero conhecê-lo!" />
         </div>
       </section>
 
-      <section className='l_aboutMe'>
+      <section className='s_aboutMe'>
         <div className="box delaySmallReveal intervalCardReveal">
-          
+
         </div>
         <div className="aboutMe intervalCardReveal">
-          <h2 className="delaySmallReveal intervalCardReveal">Sobre mim</h2>
-          <p className="delayMediumReveal intervalCardReveal">
+          <h2 className="delaySmallReveal intervalCardReveal">
+            Sobre mim
+          </h2>
+          <p className="delaySmallReveal intervalCardReveal">
             Meu nome é Alberto Albuquerque, tenho 19 anos e sou desenvolvedor Front-end Jr. <br />
             Meu início no mundo da programação se deu em 2019, onde tive meu primeiro contato com uma
             linguagem de programação: <i>C#</i>. <br />
             Me encantei pela forma de como alguns problemas recorrentes e necessários
-            podem facilmente serem resolvidos com o auxílio da tecnologia.
+            podem facilmente serem resolvidos com o auxílio da tecnologia e, desde então,
+            desenvolvo tudo que possa vir à ser útil para alguém.
           </p>
         </div>
       </section>
