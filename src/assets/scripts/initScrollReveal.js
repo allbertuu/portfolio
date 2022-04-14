3880b05304904c2c95429ddbc421266486f45369
Alberto <albertovinicius3@gmail.com>
2022-04-14 14:22:10 -0300

    update

--- src/assets/scripts/initScrollReveal.js
+++ src/assets/scripts/initScrollReveal.js
@@ -3,13 +3,13 @@ import ScrollReveal from 'scrollreveal'
 export function initScrollReveal() {
   const sr = ScrollReveal({
     origin: "top",
-    distance: "50px",
+    distance: "60px",
     duration: 2000,
   });
   
   sr.reveal(".delaySmallReveal", { delay: 200 });
-  sr.reveal(".delayMediumReveal", { delay: 300 });
-  sr.reveal(".delayLargeReveal", { delay: 400 });
-  sr.reveal(".delayExtraBigReveal", { delay: 600 });
-  sr.reveal(".intervalCardReveal", { interval: 400 });
+  sr.reveal(".delayMediumReveal", { delay: 400 });
+  sr.reveal(".delayLargeReveal", { delay: 600 });
+  sr.reveal(".delayExtraBigReveal", { delay: 800 });
+  sr.reveal(".intervalCardReveal", { interval: 800 });
 }
