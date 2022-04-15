09972bc9a23abf32ba51c7f6c55245039993bfcb
Alberto <albertovinicius3@gmail.com>
2022-04-15 12:32:28 -0300

    restructuring project

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,9 @@
+// scripts
+import { scrollTo } from '../assets/scripts/scrollTo';
 // styles
 import btn from '../assets/sass/Button.module.scss';
 
 function Button({ text, to }) {
-
-  function scrollTo(id) {
-    let el = document.getElementById(id);
-    el.scrollIntoView({
-      behavior: 'smooth',
-    })
-  }
-
   return (
     <button className={btn.btn_faixa} onClick={() => scrollTo(to)}>
       {text}
