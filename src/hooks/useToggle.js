1724b925bc793340fbaa3b553b67a76088e6c0d5
Alberto <albertovinicius3@gmail.com>
2022-04-15 14:35:58 -0300

    fix toggle

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react';
 
-export function useToggle(initialState = false) {
+export function useToggle(initialState) {
   const [isOpen, setIsOpen] = useState(initialState);
   const toggle = useCallback(() => setIsOpen(isOpen => !isOpen), []);
   return [isOpen, toggle]
