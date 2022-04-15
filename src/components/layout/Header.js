7694758a6778565e8a6ee52f75100b4868bdcd24
Alberto <albertovinicius3@gmail.com>
2022-04-15 14:36:16 -0300

    apply clean code

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,7 +8,7 @@ import btn from '../../assets/sass/Button.module.scss';
 
 function Header() {
 
-  const [isMenuOpen, toggle] = useToggle();
+  const [isMenuOpen, toggle] = useToggle(false);
 
   const sectionList = [
     {
@@ -37,28 +37,17 @@ function Header() {
       </div>
 
       <nav>
-        {isMenuOpen &&
-          <div className="fixed inset-0 c_menu_sm">
-            <button className='text-white absolute' onClick={toggle}>Close</button>
-            <ul className="flex flex-col items-center justify-center h-full list-none gap-6">
-              {sectionList.map((section) => (
-                <li onClick={() => scrollToView(section.id)}
-                  className={isMenuOpen ? `${btn.btn_li} ${btn.sm}` : `${btn.btn_li} ${btn.md}`}>
-                  {section.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        }
-        <ul className="hidden md:flex list-none gap-4">
-          {sectionList.map((section) => (
-            <li onClick={() => scrollToView(section.id)}
-              className={isMenuOpen ? `${btn.btn_li} ${btn.sm}` : `${btn.btn_li} ${btn.md}`}>
-              {section.name}
-            </li>
-          ))}
-        </ul>
-
+        <div className={isMenuOpen ? "fixed inset-0 c_menu_sm" : undefined}>
+          <button className={isMenuOpen ? "text-white absolute" : "hidden"} onClick={toggle}>Close</button>
+          <ul className={isMenuOpen ? "flex flex-col items-center justify-center h-full list-none gap-6" : "hidden md:flex list-none gap-4"}>
+            {sectionList.map((section) => (
+              <li onClick={() => scrollToView(section.id)}
+                className={isMenuOpen ? `${btn.btn_li} ${btn.sm}` : `${btn.btn_li} ${btn.md}`}>
+                {section.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
       <button className="md:hidden" onClick={toggle}>Menu</button>
     </header>
