723794a8c52e4e63534486f4105d889b9fec2732
Alberto <albertovinicius3@gmail.com>
2022-04-15 11:33:40 -0300

    remove RRD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
 import Home from './pages/Home';
 import Projetos from './pages/Projetos';
 import Header from './components/layout/Header';
@@ -12,15 +6,11 @@ import './App.scss';
 
 function App() {
   return (
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projetos />} />
-        {/* <Route path="/skills" element={<Habilidades />} /> */}
-      </Routes>
+    <>
+      <Header />
+
       <footer></footer>
-    </Router>
+    </>
   );
 }
 
