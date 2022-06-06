e7d68d0ae21951e4c7781929a6d2c8ce7d977c21
Alberto <albertovinicius3@gmail.com>
2022-06-06 16:15:12 -0300

    feat: skills cards

---
+++ src/components/SkillCardIcon/index.tsx
@@ -0,0 +1,17 @@
+type SkillCardIconProps = {
+  id: string,
+  path: string,
+  viewBox: string
+}
+
+export default function SkillCardIcon({ id, path, viewBox }: SkillCardIconProps) {
+  return (
+    <svg
+      id={id}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="rgb(218, 9, 65)"
+      viewBox={viewBox}>
+      <path d={path} />
+    </svg>
+  );
+}
