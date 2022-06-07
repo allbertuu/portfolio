311f4702c0d869597a8f7b7549ed952b37f0ee73
Alberto <albertovinicius3@gmail.com>
2022-06-07 10:41:20 -0300

    remove: unused file

--- src/hooks/useToggle.js
+++
@@ -1,7 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export function useToggle(initialState) {
-  const [isOpen, setIsOpen] = useState(initialState);
-  const toggle = useCallback(() => setIsOpen(isOpen => !isOpen), []);
-  return [isOpen, toggle]
-}
