0f3d0231845382a86a368aa47a8fc930f26c6ded
Alberto <albertovinicius3@gmail.com>
2022-07-08 13:10:15 -0300

    feat: add colors module for styled components

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: "#da0941",
+  primaryDark: "#b80c3a",
+  primaryDarker: "#a10831d0",
+
+  secondary: "#041a27",
+  secondaryDark: "#041722",
+  white90: "rgba(255, 255, 255, 0.90)",
+
+  bgPrimary: "linear-gradient(70deg, #611d2c, #da0941);",
+  bgSecondary: "linear-gradient(240deg, #041722, #041a27)",
+};
